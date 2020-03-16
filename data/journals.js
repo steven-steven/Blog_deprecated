259fc5f2afc06365cfc177a43f417587c118a25a
Steven Steven <s3@uwaterloo.ca>
2020-03-16 16:55:02 -0400

    add archived entries

--- data/journals.js
+++ data/journals.js
@@ -33,11 +33,6 @@ export default {
         title: 'No Last Name'
     },
     {
-        date: 'October 27, 2019',
-        slug: 'two-weeks-of-rock-climbing-yet-still-too-weak',
-        title: 'Two Weeks of Rock Climbing, Yet Still Too Weak'
-    },
-    {
         date: 'October 20, 2019',
         slug: 'code-brakedown-maze-generator',
         title: 'Code Breakdown - Maze Generator'
@@ -46,6 +41,13 @@ export default {
         date: 'October 14, 2019',
         slug: 'creating-this-site',
         title: 'First Blog Post - Creating this site'
+    }
+  ],
+  archive: [
+    {
+        date: 'October 27, 2019',
+        slug: 'two-weeks-of-rock-climbing-yet-still-too-weak',
+        title: 'Two Weeks of Rock Climbing, Yet Still Too Weak'
     }
   ]
 };
