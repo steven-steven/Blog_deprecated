c4e8ab507ca281644a5bbcb280f9f4b6fc11b651
Steven Steven <s3@uwaterloo.ca>
2020-04-29 02:57:34 -0400

    Add entry - when storm ends

--- data/journals.js
+++ data/journals.js
@@ -1,53 +1,58 @@
 export default {
-  2020: [
-    {
-        date: 'March 12, 2020',
-        slug: 'indexing-with-spatial-search-algorithm',
-        title: 'Indexing with Spatial Search Algorithm'
-    },
-    {
-        date: 'February 25, 2020',
-        slug: 'discovering-notion',
-        title: 'Discovering Notion'
-    }
-  ],
-  2019: [
-    {
-        date: 'December 27, 2019',
-        slug: 'learning-okr',
-        title: 'Learning OKR'
-    },
-    {
-        date: 'December 26, 2019',
-        slug: 'a-retrospective-of-my-4-months-sprint-at-Loblaw-Digital',
-        title: 'A Retrospective of my 4 months sprint at Loblaw Digital'
-    },
-    {
-        date: 'November 11, 2019',
-        slug: 'debug-how-nextjs-works-with-redux',
-        title: 'Debug how Next.js works with Redux'
-    },
-    {
-        date: 'November 03, 2019',
-        slug: 'no-last-name',
-        title: 'No Last Name'
-    },
-    {
-        date: 'October 20, 2019',
-        slug: 'code-brakedown-maze-generator',
-        title: 'Code Breakdown - Maze Generator'
-    },
-    {
-        date: 'October 14, 2019',
-        slug: 'creating-this-site',
-        title: 'First Blog Post - Creating this site'
-    }
-  ],
-  archive: [
-    {
-        date: 'October 27, 2019',
-        slug: 'two-weeks-of-rock-climbing-yet-still-too-weak',
-        title: 'Two Weeks of Rock Climbing, Yet Still Too Weak'
-    }
-  ]
+    2020: [
+        {
+            date: 'April 22, 2020',
+            slug: 'when-the-storm-ends',
+            title: 'When The Storm Ends (a 3B term recap)'
+        },
+        {
+            date: 'March 12, 2020',
+            slug: 'indexing-with-spatial-search-algorithm',
+            title: 'Indexing with Spatial Search Algorithm'
+        },
+        {
+            date: 'February 25, 2020',
+            slug: 'discovering-notion',
+            title: 'Discovering Notion'
+        }
+    ],
+    2019: [
+        {
+            date: 'December 27, 2019',
+            slug: 'learning-okr',
+            title: 'Learning OKR'
+        },
+        {
+            date: 'December 26, 2019',
+            slug: 'a-retrospective-of-my-4-months-sprint-at-Loblaw-Digital',
+            title: 'A Retrospective of my 4 months sprint at Loblaw Digital'
+        },
+        {
+            date: 'November 11, 2019',
+            slug: 'debug-how-nextjs-works-with-redux',
+            title: 'Debug how Next.js works with Redux'
+        },
+        {
+            date: 'November 03, 2019',
+            slug: 'no-last-name',
+            title: 'No Last Name'
+        },
+        {
+            date: 'October 20, 2019',
+            slug: 'code-brakedown-maze-generator',
+            title: 'Code Breakdown - Maze Generator'
+        },
+        {
+            date: 'October 14, 2019',
+            slug: 'creating-this-site',
+            title: 'First Blog Post - Creating this site'
+        }
+    ],
+    archive: [
+        {
+            date: 'October 27, 2019',
+            slug: 'two-weeks-of-rock-climbing-yet-still-too-weak',
+            title: 'Two Weeks of Rock Climbing, Yet Still Too Weak'
+        }
+    ]
 };
